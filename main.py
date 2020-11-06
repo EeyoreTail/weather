49b380c0190d65bf431c4bcbdb1278aa9ef32bfb
EeyoreTail <515870653@qq.com>
2020-11-06 17:18:24 +0800

    增加了注释

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import get_weather
 import forecast_weather
 
+#天气预报
 filename = 'weather.json'
 url = 'http://t.weather.itboy.net/api/weather/city/101020100'
 get_weather.get_weather_json(filename, url)
